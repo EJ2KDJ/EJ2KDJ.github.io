c63733e4e24e3be8ff0322e3465e65b04e998db6
EJ2KDJ <65149114+EJ2KDJ@users.noreply.github.com>
2020-08-19 10:07:11 +0800

    update

--- contact.php
+++ contact.php
@@ -5,6 +5,13 @@ if($_POST["submit"]) {
     $sender=$_POST["sender"];
     $senderEmail=$_POST["senderEmail"]
     $message=$_POST["message"]
+
+    $mailBody="Name: $sender/nEmail:
+   $senderEmail/n/n$message";
+
+       mail($recipient, $subject, $mailBody, "from: $sender <$senderEmail>");
+
+       $thankYou="<p> thank you! Your message has been sent.</p>";
 }
 
 
